0a332b8deadce121baa962b67234b8c78ebf60d0
hh <11111@123.com>
2018-06-30 16:33:26 +0800

    增加db.sql

--- src/cn/yd/xm/utils/JdbcUtils.java
+++ src/cn/yd/xm/utils/JdbcUtils.java
@@ -1,5 +1,37 @@
 package cn.yd.xm.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
 // 主要用来完成数据库连接(资源释放)
 public class JdbcUtils {
+	public static void main(String[] args) {
+		JdbcUtils utils =new JdbcUtils();
+		utils.getConnection();
+	}
+	
+	public Connection getConnection() {
+		try {
+			Connection conn =DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","root","123");
+		  return conn;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+			}
+	}
+	
+	static {
+		System.out.println("加载驱动");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+		}
+	}
 	
 }
